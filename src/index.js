50bc21aaf61cf00387ebd37e37b316594ed4c036
MarcinZbor <marcinzb@gazeta.pl>
2023-06-19 10:23:03 +0200

    TestCommit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { App } from 'components/App';
 import './index.css';
+// traaa
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
