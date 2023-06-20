31509f8c6be3fd6e0d32be2e1688ea8983d06ceb
MarcinZbor <marcinzb@gazeta.pl>
2023-06-20 08:37:47 +0200

    Api

--- src/api/api.js
+++ src/api/api.js
@@ -2,21 +2,15 @@ import axios from 'axios';
 
 const CONTACTS_URL = 'contacts';
 
-const instance = axios.create({
-  baseURL: 'https://648abf8017f1536d65e99681.mockapi.io',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
 
 export const selectContacts = () => {
-    return instance.get(`/${CONTACTS_URL}`)
+    return axios.get(`/${CONTACTS_URL}`)
 }
 
 export const addContact = data => {
-    return instance.post(`/${CONTACTS_URL}`, data)
+    return axios.post(`/${CONTACTS_URL}`, data)
 }
 
 export const deleteContact = id => {
-    return instance.delete(`/${CONTACTS_URL}/${id}`)
+    return axios.delete(`/${CONTACTS_URL}/${id}`)
 }
